bea77e13eb685322a405a11621cc32fb32a1b953
antonio marcos <marcosgeo@yahoo.com>
2023-01-20 07:30:52 -0300

    Adding a Router to handle paths

--- bumbo/app.py
+++ bumbo/app.py
@@ -5,3 +5,15 @@ from bumbo.api import API
 
 app = API()
 
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+    
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
+
+
