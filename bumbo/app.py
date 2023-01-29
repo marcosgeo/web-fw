f3651906112aa763c77995e60e16d87df2465ac4
antonio marcos <marcosgeo@yahoo.com>
2023-01-29 09:49:15 -0300

    just small formatting

--- bumbo/app.py
+++ bumbo/app.py
@@ -7,23 +7,6 @@ from bumbo.middleware import Middleware
 app = API()
 
 
-def custom_exception_handler(request, response, exception_cls):
-    response.text = str(exception_cls)
-
-app.add_exception_handler(custom_exception_handler)
-
-# custom middleware
-class SimpleCustomMiddleware(Middleware):
-    def process_request(self, req):
-        print("Processing request", req.url)
-
-    def process_response(self, req, resp):
-        print("Processing response", req.url, resp)
-
-app.add_middleware(SimpleCustomMiddleware)
-
-
-
 @app.route("/home")
 def home(request, response):
     response.text = "\nHello from the HOME page\n\n"
@@ -43,29 +26,6 @@ def greeting(request, response, name):
 def sum(request, response, num_1, num_2):
     total = int(num_1) + int(num_2)
     response.text = f"\n{num_1} + {num_2} = {total}\n\n"
-
-
-@app.route("/book")
-class BookResource:
-    def get(self, request, response):
-        response.text = "\nBooks Page\n\n"
-    
-    def post(self, request, response):
-        response.text = "\nEndpoint to create a book\n\n"
-    
-    
-    def put(self, request, response):
-        response.text = "\nEndpoint to update a book\n\n"
-
-    def delete(self, request, response):
-        response.text = "\nEnpoint to delete a book\n\n"
-
-
-def handler(req, resp):
-    resp.text = "\nDjango-like handler\n\n"
-
-app.add_route("/sample", handler)
-
 
 
 @app.route("/exception")
@@ -89,5 +49,58 @@ def json_handler(req, resp):
 @app.route("/text")
 def text_handler(req, resp):
     resp.text = "This is a simple text"
+
+
+
+# class-based handler
+
+
+@app.route("/book")
+class BookResource:
+    def get(self, request, response):
+        response.text = "\nBooks Page\n\n"
+
+    def post(self, request, response):
+        response.text = "\nEndpoint to create a book\n\n"
+
+    def put(self, request, response):
+        response.text = "\nEndpoint to update a book\n\n"
+
+    def delete(self, request, response):
+        response.text = "\nEnpoint to delete a book\n\n"
+
+
+
+# django-like handlers
+
+
+def handler(req, resp):
+    resp.text = "\nDjango-like handler\n\n"
+
+app.add_route("/sample", handler)
+
+
+
+# exception handler
+
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+app.add_exception_handler(custom_exception_handler)
+
+
+
+# custom middleware
+
+
+class SimpleCustomMiddleware(Middleware):
+    def process_request(self, req):
+        print("Processing request", req.url)
+
+    def process_response(self, req, resp):
+        print("Processing response", req.url, resp)
+
+app.add_middleware(SimpleCustomMiddleware)
 
 
